05d60295f67219e2019ff6b3647bb1f7ea12a921
laboon <laboon@gmail.com>
2018-09-08 18:04:23 -0400

    add comments

--- sample_code/hash_pointers/Account.java
+++ sample_code/hash_pointers/Account.java
@@ -1,8 +1,25 @@
+/**
+ * A relatively generic class which implements HashableObject.
+ * An account consists of a name and how many dollars that account
+ * has.
+ */
+
 public class Account implements HashableObject {
+
+    // Name associated with account
 
     public String _name = "";
 
+    // Number of dollars in accounts
+
     public String _dollars = "";
+
+    /**
+     * Return data stored in this object as a string.
+     * Think of it as an alternative to .toString().
+     * Format is "name/dollars".
+     * @return String representation of this object
+     */
 
     public String getDataAsString() {
 	StringBuilder toReturn = new StringBuilder();
@@ -11,6 +28,14 @@ public class Account implements HashableObject {
 	toReturn.append(_dollars);
 	return toReturn.toString();
     }
+
+    /**
+     * Constructor.
+     * Creates a new account with a name and dollars associated
+     * with the account.
+     * @param name - name of account owner
+     * @param dollars - dollars in account
+     */
 
     public Account(String name, String dollars) {
 	_name = name;
--- sample_code/hash_pointers/HashPointer.java
+++ sample_code/hash_pointers/HashPointer.java
@@ -8,9 +8,20 @@ import java.security.NoSuchAlgorithmException;
 
 public class HashPointer {
 
+    // Reference to some object
+
     private HashableObject _reference;
 
+    // The hash of _reference
+
     private String _hash;
+
+    /**
+     * Given some arbitrary byte array bytes, convert it to a hex string.
+     * Example: [0xFF, 0xA0, 0x01] -> "FFA001"
+     * @param bytes arbitrary-length array of bytes
+     * @return String hex string version of byte array
+     */
 
     private static String convertBytesToHexString(byte[] bytes) {
 	StringBuffer toReturn = new StringBuffer();
@@ -20,6 +31,14 @@ public class HashPointer {
 	}
 	return toReturn.toString();
     }
+
+    /**
+     * Given some HashableObject, determine a string representation of it
+     * (all HashableObjects have this as part of the interface), and
+     * return the hash of the string representation.
+     * @param x Some object which meets the HashableObject interface
+     * @return String version of the hash of that object's data
+     */
 
     public static String calculateHash(HashableObject x) {
 	String dataAsString = x.getDataAsString();
@@ -38,17 +57,42 @@ public class HashPointer {
 
     }
 
+    /**
+     * Accessor to return _reference (read-only, no mutator)
+     * @return _reference object
+     */
+
     public HashableObject getReference() {
 	return _reference;
     }
+
+    /**
+     * Accessor to return _hash (read-only, no mutator)
+     * @return _hash
+     */
 
     public String getHash() {
 	return _hash;
     }
 
+    /**
+     * Called to determine whether or not this hash pointer is valid
+     * (i.e., the data of the object to which it points has not been
+     * tampered with).
+     * @return boolean - true if valid, false if not
+     */
+
     public boolean referenceValid() {
 	return _hash.equals(calculateHash(_reference));
     }
+
+    /**
+     * Constructor.
+     * Given some HashableObject, create a pointer (reference) to
+     * it and calculate its initial hash value.
+     * After creation, both of these values are read-only.
+     * @param reference Some object which implements HashableObject.
+     */
 
     public HashPointer(HashableObject reference) {
 	_reference = reference;
