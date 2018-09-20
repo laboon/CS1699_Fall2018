9f9aca483dc1ebea59fcc23b799cb7319a372556
laboon <laboon@gmail.com>
2018-09-20 14:44:39 -0400

    add miner sample code

---
+++ sample_code/mining/Miner.java
@@ -0,0 +1,108 @@
+import java.math.BigInteger;
+
+/**
+ * This program will attempt to find a nonce such that
+ * H(nonce || block) < target
+ * where the hashing function is SHA-256, block is argument 0, target is
+ * argument 1 (interpreted as a hex string)
+ * Example:
+ * (24425) $ java Miner HuzzahForBitcoin 1000000000000000000000000000000000000000000000000000000000000
+ * Hash rate = 51102 hashes per second
+ * Nonce       = c79d
+ * Full block  = 000000000000000000000000000000000000000000000000000000000000c79dHuzzahForBitcoin
+ * Hash(block) = 00003556921b058ee69dff14ce2fa0c35791f5d3f54088c33768a300ccc7d261
+ */
+
+public class Miner {
+
+    /**
+     * This increments our String nonce by accepting a String version
+     * and returning a String version.  For example:
+     * "000A" -> "000B"
+     * "FFFE" -> "FFFF"
+     * @param nonce initial nonce
+     * @return nonce incremented by one in string form
+     */
+    public static String incrementStringNonce(String nonce) {
+	BigInteger bi = new BigInteger(nonce, 16);
+	bi = bi.add(BigInteger.ONE);
+	return bi.toString(16);
+    }
+
+    /**
+     * Prepend a string with 0's until it is of length n.
+     * Useful for printing out hash results.
+     * @param str String to prepend 0's to
+     * @param n correct size of string after being padded
+     * @return String str left-padded with 0's
+     */
+    public static String leftPad(String str, int n) {
+	return String.format("%1$" + n + "s", str).replace(' ', '0');
+    }
+
+    /**
+     * Given a start time and end time in nanoseconds (courtesy of System.nanoTime),
+     * and a number of hashes complete in this time, print out the number of hashes
+     * per second.
+     * @param numHashes - number of hashes completed
+     * @param startTime - time hashing started
+     * @param endTime - time hashing ended
+     */
+    public static void printHashRate(BigInteger numHashes,
+				     long startTime,
+				     long endTime) {
+	long timeDiff = endTime - startTime;
+	long seconds = timeDiff / 1000000000;
+	BigInteger time = new BigInteger ((Long.valueOf(seconds)).toString());
+	BigInteger hashesPerSecond = numHashes.divide(time);
+	System.out.println("Hash rate = " + hashesPerSecond + " hashes per second");
+
+    }
+
+    public static void main(String[] args) {
+	if (args.length != 2) {
+	    System.err.println("Usage: *block_string* *target_in_hex*");
+	    System.exit(1);
+	}
+	String nonce = "0";
+	String block = args[0];
+	BigInteger target = new BigInteger(args[1], 16);
+	boolean lookingForTarget = true;
+	String concat = "";
+	BigInteger hash = null;
+
+	BigInteger numHashes = new BigInteger("0");
+	long startTime = System.nanoTime();
+
+	// Keep looping until we find target...
+	while (lookingForTarget) {
+
+	    // Increment number of hashes tried
+	    numHashes = numHashes.add(BigInteger.ONE);
+	    // Concatenate our nonce and our block
+	    concat = (leftPad(nonce, 64)) + block;
+
+	    // Uncomment to see failed attempts
+	    // System.out.println("Trying: " + concat);
+
+	    // Calculate the hash.  Kept in a BigInteger since we will want to compare it.
+	    hash = Sha256Hash.hashBigInteger(concat);
+
+	    // If our hash is less than the target, we have succeeded
+	    if (hash.compareTo(target) == -1) {
+		long endTime = System.nanoTime();
+		printHashRate(numHashes, startTime, endTime);
+		lookingForTarget = false;
+		System.out.println("Nonce       = " + nonce);
+		System.out.println("Full block  = " + concat);
+		System.out.println("Hash(block) = " + leftPad(hash.toString(16), 64));
+	    } else {
+		// Uncomment to see failed attempts
+		// System.out.println("Fail, hash "
+		// 		   + leftPad(hash.toString(16), 64) + " >= "
+		// 		   + leftPad(target.toString(16), 64));
+		nonce = incrementStringNonce(nonce);
+	    }
+	}
+    }
+}
---
+++ sample_code/mining/Sha256Hash.java
@@ -0,0 +1,63 @@
+// These imports allow us to do SHA-256 hashing - we need not only MessageDigest
+// which contains the algorithm itself, but the ancillary class StandardCharsets and
+// NoSuchAlgorithmException for feeding in data and running it
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256Hash {
+
+    /**
+     * Given some arbitrary byte array bytes, convert it to a hex string.
+     * Example: [0xFF, 0xA0, 0x01] -> "FFA001"
+     * @param bytes arbitrary-length array of bytes
+     * @return String hex string version of byte array
+     */
+
+    private static String convertBytesToHexString(byte[] bytes) {
+	StringBuffer toReturn = new StringBuffer();
+	for (int j = 0; j < bytes.length; j++) {
+	    String hexit = String.format("%02x", bytes[j]);
+	    toReturn.append(hexit);
+	}
+	return toReturn.toString();
+    }
+
+    /**
+     * Given some string, return the SHA256 hash of it.
+     * @param x Arbitrary string
+     * @return String Hex version of the hash of that object's data
+     */
+
+    public static String calculateHash(String x) {
+	if (x == null) {
+	    return "0";
+	}
+	byte[] hash = null;
+	try {
+	    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	    hash = digest.digest(x.getBytes());
+	} catch (NoSuchAlgorithmException nsaex) {
+	    System.err.println("No SHA-256 algorithm found.");
+	    System.err.println("This generally should not happen...");
+	    System.exit(1);
+	}
+	return convertBytesToHexString(hash);
+
+    }
+
+    /**
+     * Given a string, returns the hash as a BigInteger
+     * @param toHash - string to hash
+     * @return BigInteger - hash of String toHash
+     * @see calculateHash method, above
+     */
+
+    public static BigInteger hashBigInteger(String toHash) {
+	return new BigInteger(calculateHash(toHash), 16);
+    }
+
+
+}
